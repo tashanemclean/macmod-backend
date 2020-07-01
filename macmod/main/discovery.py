8a7a194f4ae1a19199fbc58d555a506892fc24ac
Tashane Mclean <48889185+tashanemclean@users.noreply.github.com>
2020-06-30 21:06:58 -0400

    Update discovery.py

--- macmod/main/discovery.py
+++ macmod/main/discovery.py
@@ -4,16 +4,16 @@ import os
 from ibm_watson import DiscoveryV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
-authenticator = IAMAuthenticator('SmSeNMU__n3jThLMv60r10NGkmi7DStgaKAstabnCZVq')
+authenticator = IAMAuthenticator('<KEY>')
 discovery = DiscoveryV1(
     version='2018-08-01',
     authenticator=authenticator)
 
-discovery.set_service_url('https://api.us-south.discovery.watson.cloud.ibm.com/instances/15f4fc3b-de49-4a10-96e5-cd7033a7373e')
+discovery.set_service_url('<URL>')
 
 environments = discovery.list_environments().get_result()
 
-macmod_docs_environment_id = '91042ae5-ec41-4cf8-8dbe-45dbcd6b283b'
+macmod_docs_environment_id = '<ID>'
 
 collections = discovery.list_collections(macmod_docs_environment_id).get_result()
 macmod_collections = [x for x in collections['collections']]
